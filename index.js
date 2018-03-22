3c9c1416376cde3010c49c660264750038ec5dbe
Djordje Ungar <djordje.ungar@gmail.com>
2018-03-22 10:12:42 +0100

    Add additional URL scheme to DeviantArt's URL regex

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ hexo.extend.tag.register('oembed', function(args){
     endpoint = 'https://api.gyazo.com/api/oembed/';
   }else if(opts.url.match(/^http(s?):\/\/codepen\.io\/[\/\w]*$/)){
     endpoint = 'http://codepen.io/api/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/fav\.me\/[\/\w]*$/)){
+  }else if(opts.url.match(/^http(s?):\/\/(fav\.me\/[\/\w]*|\.deviantart\.com/art/\w+-\d+)$/)){
     endpoint = 'https://backend.deviantart.com/oembed';
   }
 

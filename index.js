ba80018ffbcc15d25bd418679d675892bfc60803
Djordje Ungar <djordje.ungar@gmail.com>
2018-03-23 22:06:48 +0100

    Keep oembed provider config in a json file

--- index.js
+++ index.js
@@ -2,35 +2,26 @@
 
 var requestpromise = require('request-promise');
 var querystring = require('querystring');
+var providers = require('./providers.json');
 
 hexo.extend.tag.register('oembed', function(args){
-  if(!args) return;
+  if (!args || !providers) return;
 
-  var opts = {
-    "url": args[0],
-    "format": "json"
+  var params = {
+    url: args[0],
+    format: "json"
   };
 
-  var endpoint = '';
-  if(opts.url.match(/^http(s?):\/\/twitter\.com\/(.*)\/(status|statuses)\/(\d+)$/)){
-    endpoint = 'https://publish.twitter.com/oembed';
-    opts["lang"] = hexo.config.language;
-  }else if(opts.url.match(/^http(s?):\/\/www\.youtube\.com\/watch\?v=[-\w]{11,}$/)){
-    endpoint = 'http://www.youtube.com/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/vimeo\.com\/\d+$/)){
-    endpoint = 'http://vimeo.com/api/oembed.json';
-  }else if(opts.url.match(/^http(s?):\/\/www\.flickr\.com\/photos\/[-@\w]+\/\d+\/$/)){
-    endpoint = 'http://www.flickr.com/services/oembed/';
-  }else if(opts.url.match(/^http(s?):\/\/(www\.)?instagram\.com\/p\/\w+\/$/)){
-    endpoint = 'https://api.instagram.com/oembed/';
-  }else if(opts.url.match(/^http(s?):\/\/gyazo\.com\/\w*$/)){
-    endpoint = 'https://api.gyazo.com/api/oembed/';
-  }else if(opts.url.match(/^http(s?):\/\/codepen\.io\/[\/\w]*$/)){
-    endpoint = 'http://codepen.io/api/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/(fav\.me\/\w+|\w+\.deviantart\.com\/art\/\w+-\d+)$/)){
-    endpoint = 'https://backend.deviantart.com/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/probe\.djordjeungar\.com\/song\/\w+$/)){
-    endpoint = 'https://probe.djordjeungar.com/oembed';
+  var endpoint = '', provider;
+  for (var i = 0; i < providers.length; i++) {
+      provider = providers[i];
+      if (params.url.match(new RegExp(provider.test))) {
+          endpoint = provider.endpoint;
+          if (provider.params) {
+            params =  Object.assign(params, provider.params);
+          }
+          break;
+      }
   }
 
   var getRequest = function(uri){
@@ -41,15 +32,15 @@ hexo.extend.tag.register('oembed', function(args){
         return JSON.parse(body);
       }
     });
-  }
+  };
 
-  var req = endpoint + '?' + querystring.stringify(opts);
+  var req = endpoint + '?' + querystring.stringify(params);
   return getRequest(req)
     .then(function(data){
       switch(data.type){
         case 'photo':
           var alt = data.title ? ' alt="'+data.title+'"' : '';
-          return '<a href="'+opts['url']+'"><img src="'+data.url+'"'+alt+'></a>';
+          return '<a href="'+params['url']+'"><img src="'+data.url+'"'+alt+'></a>';
           break;
         case 'video':
         case 'rich':
